466e5ad11ea08226dda4292252973d29482e0325
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-08-24 11:29:36 +0200

    Update rss.php

--- src/rss.php
+++ src/rss.php
@@ -1,5 +1,6 @@
 <?php header('Content-type: application/xml'); 
-
+$config = 'directories' ;
+if($config=='database') {
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'username');
 define('DB_PASSWORD', 'password');
@@ -7,6 +8,7 @@ define('DB_NAME', 'database');
  
 $conn = new PDO("mysql:host=".DB_SERVER.";port=8889;dbname=".DB_NAME, DB_USER, DB_PASSWORD);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
 echo "<rss version='2.0' xmlns:atom='http://www.w3.org/2005/Atom'>\n";
 echo "<channel>\n";
@@ -32,4 +34,33 @@ while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
 
 echo "</channel>\n";
 echo "</rss>\n";
+
+} else if($config=='directories' ){
+$path= '../outputFiles/features/';
+
+echo "<rss version='2.0' xmlns:atom='http://www.w3.org/2005/Atom'>\n";
+echo "<channel>\n";
+
+echo "<title>Demo RSS Feed</title>\n";
+echo "<description>RSS Description</description>\n";
+echo "<link>http://www.mydomain.com</link>\n";
+
+ 
+$stmt = $conn->query('SELECT * FROM news ORDER BY newsDate DESC LIMIT 10');
+while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+    
+     echo "<item>n";
+         echo "<title>$row->newsTitle</title>\n";
+         echo "<description>$row->newsDesc</description>\n";
+         echo "<pubDate>".date('D, d M Y H:i:s',strtotime($row->newsDate))." GMT</pubDate>\n";
+         echo "<link>http://www.mydomain.com/$row->newsSlug</link>\n";
+         echo "<guid>http://www.mydomain.com/$row->newsSlug</guid>\n";
+         echo "<atom:link href='http://www.mydomain.com/$row->newsSlug' rel='self' type='application/rss+xml'/>\n"
+     echo "</item>\n";
+
+}
+
+echo "</channel>\n";
+echo "</rss>\n";
+} 
 ?>
